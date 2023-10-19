c4f6498d8a79fa3ba1af4527331b0b7771c65904
Matheus Custódio <matheuscustodio2802@gmail.com>
2023-10-19 16:40:43 -0300

    Classe position criada

---
+++ src/models/Position.java
@@ -0,0 +1,32 @@
+package models;
+
+public class Position {
+    private int row;
+    private int column;
+
+    public Position() {
+    }
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public void setvalues(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    @Override
+    public String toString(){
+        return row + ", " + column;
+    }
+}
