81573f052a47e783393dd57c19505a7627d750d1
Matheus Custódio <matheuscustodio2802@gmail.com>
2023-10-20 11:48:21 -0300

    ajustes nos pacotes, criacao e impressao do tabuleiro

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,13 @@
 package application;
 
-import models.Position;
+import boardgame.Position;
+import chess.ChessMatch;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Position pos = new Position(1, 5);
-        System.out.println(pos);
+        ChessMatch match = new ChessMatch();
+        UI.printBoard(match.getPieces());
         }
 }
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -1,4 +1,4 @@
-package models;
+package boardgame;
 
 public class Position {
     private int row;
