75ecc6dbe89f6485100995d518f72acda8781cb6
Matheus Custódio <matheuscustodio2802@gmail.com>
2023-10-20 15:00:39 -0300

    limpeza de tela a cada rodada e tratamento de excecoes.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,11 @@
 package application;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -13,16 +15,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         ChessMatch match = new ChessMatch();
         while(true){
-            UI.printBoard(match.getPieces());
-            System.out.println();
-            System.out.print("Start: ");
-            ChessPosition start = UI.readChessPosition(sc);
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
+            try {
+                UI.clearScreen();
+                UI.printBoard(match.getPieces());
+                System.out.println();
+                System.out.print("Start: ");
+                ChessPosition start = UI.readChessPosition(sc);
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
 
-            ChessPiece capturedPiece = match.performChessMove(start, target);
-
+                ChessPiece capturedPiece = match.performChessMove(start, target);
+            }catch(ChessException | InputMismatchException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
 
         }
